5209d658216cfe21c5de9900865c58583a8aa8d5
LahiruBotcalm <lahiru.w@botcalm.com>
2025-10-15 10:54:12 +0530

    setup project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css"
+
+const App = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Hello, Vite + React + Tailwind!
+      </h1>
+    </div>
+  );
+};
+
+export default App;
